32e503d5c8340defc0acb637c1a2cced1e5aaf93
Camillo <cangelozzi@gmail.com>
2019-01-10 18:03:11 -0500

    create vue nav_header component

--- js/main.js
+++ js/main.js
@@ -2,13 +2,57 @@
 
   console.log("Welcome to Roku Flashback App.");
 
+  let navHeader_component = {
+    props: ["navlist"],
+    template: `
+         <header id="mainHeader">
+          <h2 class="hidden">Hamburger Navigation</h2>
+          <!-- Hamburger Navigation -->
+          <div class="navigation">
+            <input type="checkbox" class="navigation__checkbox" id="navi-toggle" />
+            <label for="navi-toggle" class="navigation__button">
+              <span class="navigation__icon">&nbsp;</span>
+            </label>
+            <div class="navigation__background">&nbsp;</div>
+            <nav class="navigation__nav">
+              <h3 class="hidden">Main Navigation</h3>
+              <ul class="navigation__list">
+                <li class="navigation__item" v-for="item in navlist">
+                        <a href="#" class="navigation__link">{{ item }}</a>
+                      </li>
+                <ul id="social">
+                  <li><i class="fas fa-search" style="color: white;"></i></li>
+                  <li>
+                            <a href="http://www.twitter.com" target="_blank" id="tw"
+                              ><i class="fab fa-twitter"></i
+                            ></a>
+                          </li>
+                  <li>
+                            <a href="http://www.instagram.com" target="_blank" id="pt"
+                              ><i class="fab fa-instagram"></i
+                            ></a>
+                          </li>
+                  <li>
+                            <a href="http://www.facebook.com" target="_blank" id="fb"
+                              ><i class="fab fa-facebook-square"></i
+                            ></a>
+                          </li>
+                </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </header>
+    `
+  };
+
   const vm = new Vue({
-    el: '#app',
+    el: "#app",
     data: {
-      welcomeMsg: "Welcome to Roku Flashback App.",
-      navList: ['Movie_FBK', 'TV_FBK', 'Music_FBK', 'Profile', 'Settings'],
+      welcomeMsg: "Welcome to Roku Flashback ",
+      navlist: ["Movies", "Television", "Music", "Profile", "Settings"],
       videodata: [],
-      singlemoviedata: [],
+      singlemoviedata: []
     },
 
     created: function() {
@@ -16,9 +60,10 @@
     },
 
     methods: {
-
       fetchMovieData(movie) {
-        url = movie ? `./includes/index.php?movie=${movie}` : './includes/index.php';
+        url = movie
+          ? `./includes/index.php?movie=${movie}`
+          : "./includes/index.php";
 
         fetch(url) // pass in the one or many query
           .then(res => res.json())
@@ -33,11 +78,14 @@
               this.videodata = data;
             }
           })
-          .catch(function (error) {
+          .catch(function(error) {
             console.log(error);
           });
       }
+    },
 
+    components: {
+      "nav-header": navHeader_component
     }
   });
 
